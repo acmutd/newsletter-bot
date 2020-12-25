dd75252790b1f6831107de17a5d70a8856b077ae
Eric Zhang <36653830+ez314@users.noreply.github.com>
2020-12-25 17:54:14 -0600

    update preview command

--- src/commands/Preview.ts
+++ src/commands/Preview.ts
@@ -1,4 +1,9 @@
+import { MessageEmbed } from "discord.js";
 import Command, { CommandContext } from "../structures/Command";
+import { SpreadsheetEvent } from "../structures/managers/SpreadsheetManager";
+import { OrgWithEvents } from "../structures/services/NewsletterService"
+
+
 
 export default class PreviewCommand extends Command {
     constructor() {
@@ -21,14 +26,56 @@ export default class PreviewCommand extends Command {
 
         const startTime = new Date().getTime();
 
-        // msg.channel.send(
-        //     await client.services.newsletter.buildOrgEmbed(args[0])
-        // );
+        // re-cache
+        await client.spreadsheet.fetchAllOrgs();
+
+        const org = client.spreadsheet.orgs.array().find((org) => org.abbr.toLowerCase() == args[0].toLowerCase())
+
+        //console.log(JSON.stringify(client.spreadsheet.orgs.array(), null, 2))
+
+        // error and return if org doesn't exist in the org key (sheet 0)
+        if (org == undefined) {
+            await client.response.emit(
+                msg.channel,
+                `The org \`${args[0]}\` could not be found.`
+            );
+            return;
+        }
+
+        let events = await client.spreadsheet.fetchEvents(org.abbr, 7);
+
+        if (events.length < 1) {
+            await client.response.emit(
+                msg.channel,
+                `No events were found for \`${args[0]}\` in the upcoming week.`
+            )
+        }
+
+
+        var addedID: SpreadsheetEvent[] = [];
+        events.forEach((e, count) => {
+            addedID.push({ ...e, id: count+1 });
+        });
+
+        events = [...addedID];
+        const orgWithEvents: OrgWithEvents = { events, org };
+        
+        const embed = client.services.newsletter.buildOrgEmbed(orgWithEvents)
+
+
+        msg.channel.send(embed);
 
         msg.channel.send(
-            `Preview generated in ${
-                (new Date().getTime() - startTime) / 1000
-            } seconds.`
+            new MessageEmbed({
+                color: '#EEEEEE',
+                description: 'Please **DO NOT RSVP** based on these event numbers. This is purely a preview.',
+                footer: {
+                    text: `Preview generated in ${
+                        (new Date().getTime() - startTime) / 1000
+                    } seconds.`
+                }
+            })
+            
         );
     }
 }
